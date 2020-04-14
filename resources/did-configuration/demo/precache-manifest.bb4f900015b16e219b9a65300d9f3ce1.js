15d1984af526de773b9a22a1f30f6c0b5b1e63e9
Orie Steele <orie@transmute.industries>
2020-04-13 22:00:34 -0500

    test demo

---
+++ resources/did-configuration/demo/precache-manifest.bb4f900015b16e219b9a65300d9f3ce1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feeae4f797d9171cc6b580c20ea247f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "893bf12b8c9d57e93aea",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6195427ae0b5117476a8",
+    "url": "/static/js/2.4578df0a.chunk.js"
+  },
+  {
+    "revision": "1ee6c4af3e1e4eded42613c7f5df8020",
+    "url": "/static/js/2.4578df0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "893bf12b8c9d57e93aea",
+    "url": "/static/js/main.1005bcb1.chunk.js"
+  },
+  {
+    "revision": "6a504397962e5d648a3a",
+    "url": "/static/js/runtime-main.279e03ee.js"
+  }
+]);
--- resources/did-configuration/demo/service-worker.js
+++ resources/did-configuration/demo/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/.well-known/resources/did-configuration/demo/index.html/precache-manifest.1dff071e36f5df19ae67900b5d5bfa46.js"
+  "/precache-manifest.bb4f900015b16e219b9a65300d9f3ce1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/.well-known/resources/did-configuration/demo/index.html/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
